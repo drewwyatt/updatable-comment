a40cd5546257bcb68785b61b713349295957a96d
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-09-18 19:49:44 -0400

    add input strategy

--- src/inputs.ts
+++ src/inputs.ts
@@ -4,8 +4,31 @@ enum Input {
   Comment = 'comment',
   Id = 'id',
   GithubToken = 'github-token',
+  Strategy = 'strategy',
+}
+
+export enum Strategy {
+  Update = 'update',
+  Delete = 'delete',
+}
+
+function assertIsStrategy(input: string): asserts input is Strategy {
+  if (![Strategy.Delete, Strategy.Update].includes(input as Strategy)) {
+    throw new Error(`found invalud inputs.strategy value of "${input}"
+    allowed values are: ${[Object.values(Strategy).join(' | ')]}
+    `)
+  }
+}
+
+const parseStrategy = (input: string): Strategy => {
+  const lowercased = input.toLowerCase()
+  assertIsStrategy(lowercased)
+  return lowercased
 }
 
 export const comment = getInput(Input.Comment, { required: true })
 export const id = getInput(Input.Id)
 export const githubToken = getInput(Input.GithubToken, { required: true })
+export const strategy = parseStrategy(
+  getInput(Input.Strategy, { required: true, trimWhitespace: true }),
+)
