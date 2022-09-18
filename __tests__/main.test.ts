c989527f44f8c557ec2e8b925106bdd8cda2b33a
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-09-18 00:17:33 -0400

    oh that's why it's like that.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,6 +3,7 @@ import * as cp from 'child_process'
 import * as path from 'path'
 import { test } from '@jest/globals'
 
+// shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['GITHUB_REPOSITORY'] = 'drewwyatt/updatable-comment'
   process.env['INPUT_COMMENT'] = 'lol'
